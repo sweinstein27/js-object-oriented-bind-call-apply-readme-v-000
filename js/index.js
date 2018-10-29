57c1010b23810bcc855afac90b0159a3d8adb954
Scott Weinstein <sweinstein27@gmail.com>
2018-10-29 16:23:46 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // use this to copy code snippets or us your browser's console
+let sally = { name: 'Sally' };
+
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
